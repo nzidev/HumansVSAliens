d8e0be65846f3fd45f0376e87eef48cf2724c78f
nzidev <39884860+nzidev@users.noreply.github.com>
2019-07-22 23:48:52 +0300

    FirstChanges
    Add first monster and hit click

---
+++ HumansInAliensWorld/Assets/Scripts/HealthHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHelper : MonoBehaviour
+{
+    public int MaxHealth = 100; //Всего жизней
+    public int Health = 100;    //Текущие жизни
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHit(int damage) //получаем урон
+    {
+        int health = Health - damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        
+        Health=health;
+    }
+}
---
+++ HumansInAliensWorld/Assets/Scripts/HitHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("Hit");
+        gameObject.GetComponent<HealthHelper>().GetHit(10);
+        
+    }
+
+}
